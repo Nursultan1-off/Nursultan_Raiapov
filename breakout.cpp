6af2fe7c09dbf5a1e020f8f0c8aa29c0e683edee
Nursultan Raiapov <nursultanraiapov@gmail.com>
2025-12-22 02:17:04 +0600

    Game is done

--- breakout.cpp
+++ breakout.cpp
@@ -4,40 +4,64 @@
 #include "graphics.h"
 #include "level.h"
 #include "paddle.h"
-
 #include "raylib.h"
 
 void update()
 {
-    // TODO
-
-    if (IsKeyPressed(KEY_ESCAPE)) {
-        game_state = paused_state;
-    }
-    if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) {
-        move_paddle(-paddle_speed);
-    }
-    if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) {
-        move_paddle(paddle_speed);
-    }
-    move_ball();
-    if (!is_ball_inside_level()) {
-        load_level();
-        PlaySound(lose_sound);
-    } else if (current_level_blocks == 0) {
-        load_level(1);
-        PlaySound(win_sound);
+    switch (game_state) {
+    case menu_state:
+        if (IsKeyPressed(KEY_ENTER)) game_state = in_game_state;
+        break;
+    case in_game_state:
+        if (IsKeyPressed(KEY_SPACE)) {
+            game_state = paused_state;
+        }
+        if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) {
+            move_paddle(-paddle_speed);
+        }
+        if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) {
+            move_paddle(paddle_speed);
+        }
+        move_ball();
+        if ( !is_ball_inside_level()) {
+            load_level();
+            PlaySound(lose_sound);
+        } else if (current_level_blocks == 0) {
+            load_level(1);
+        }
+        break;
+    case paused_state:
+        if (IsKeyPressed(KEY_SPACE)) {
+            game_state = in_game_state;
+        }
+        break;
+    case victory_state:
+        if (IsKeyPressed(KEY_ENTER)) {
+            game_state = menu_state;
+        }
+        break;
     }
 }
 
 void draw()
 {
-    // TODO
-
-    draw_level();
-    draw_paddle();
-    draw_ball();
-    draw_ui();
+    switch (game_state) {
+    case menu_state:
+        draw_menu();
+        break;
+    case in_game_state:
+        draw_level();
+        draw_paddle();
+        draw_ball();
+        draw_ui();
+        break;
+    case paused_state:
+        draw_pause_menu();
+        break;
+    case victory_state:
+        draw_victory_menu();
+        break;
+    }
 }
 
 int main()
@@ -51,12 +75,11 @@ int main()
     load_level();
     load_sounds();
 
+
     while (!WindowShouldClose()) {
         BeginDrawing();
-
         draw();
         update();
-
         EndDrawing();
     }
     CloseWindow();
